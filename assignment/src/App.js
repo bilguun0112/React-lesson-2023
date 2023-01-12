1de2fb8dd4c74dcf1131c69337bd6d8d49dbd493
Morning <morning@mstars-lab2-10s-iMac.local>
2023-01-12 13:26:40 +0800

    assignment

---
+++ assignment/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CreateFunction from './components/createFunction';
+
+function App() {
+  return (
+    <div className="App">
+     <CreateFunction />
+    </div>
+  );
+}
+
+export default App;
