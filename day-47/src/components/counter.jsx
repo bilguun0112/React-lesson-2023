3bfca5b54b2c87c1f6fb1e1f05c6c0d55e8fc90d
Morning <morning@mstars-lab2-10s-iMac.local>
2023-01-20 09:37:58 +0800

    day 47 mini exam

---
+++ day-47/src/components/counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <style type="text/css">
+                {`
+                .counter:hover {
+                background: #ffffd8
+                   }
+                 `}
+            </style>
+            <div className="counter p-2 w-25" >
+                <div className="p-3 fs-1">
+                    {count}
+                </div>
+                <button className="btn btn-secondary" onClick={() => {
+                    setCount(count + 1)
+                }}>
+                    Add One
+                </button>
+            </div>
+        </div>
+    )
+}
