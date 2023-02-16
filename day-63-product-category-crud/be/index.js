30bef396f3bf1dfa4cec1d31fd3f61c07d79c6a1
Morning <morning@mstars-lab2-10s-iMac.local>
2023-02-16 11:53:53 +0800

    day 63

--- day-63-product-category-crud/be/index.js
+++ day-63-product-category-crud/be/index.js
@@ -1,69 +1,130 @@
-console.log('Day 63 product category CRUD')
+console.log("Day-63 Category CRUD");
 
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
-const { request } = require('http')
-const { log } = require('console')
-const { response } = require('express')
+/// import necessary packages
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const { request } = require("http");
+const { response } = require("express");
 
-const app = express()
-const PORT = 8080
+const PORT = 8080;
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.route('/category')
-    .post((request, response) => {
-        const body = request.body
-        console.log(body)
+app
+  .route("/category")
+  .post((request, response) => {
+    const body = request.body;
+    console.log(body);
+    const isEdit = body.isEdit;
+    const categoryData = fs.readFileSync("./data/categories.json", {
+      encoding: "utf-8",
+      flag: "r",
+    });
 
-        const categoryData = fs.readFileSync("./data/categories.json", {
-            encoding: 'utf-8',
-            flag: 'r'
-        })
-        const categoryDataObj = JSON.parse(categoryData)
+    const categoryDataObj = JSON.parse(categoryData);
 
-        const newCategory = {
-            id: Date.now().toString().slice(-4),
-            name: body.catName
+    if (isEdit) {
+      categoryDataObj.map(category => {
+        if (category.id == body.categoryId) {
+          category.name = body.categoryName
         }
-        categoryDataObj.push(newCategory)
+        return category
+      });
+    } else {
+      const newCategory = {
+        id: Date.now().toString(),
+        name: body.categoryName,
+      };
+      categoryDataObj.push(newCategory);
+    }
 
-        const writeCategoryData = fs.writeFileSync("./data/categories.json", JSON.stringify(categoryDataObj))
+    const writeCategoryData = fs.writeFileSync(
+      "./data/categories.json",
+      JSON.stringify(categoryDataObj)
+    );
+    console.log(writeCategoryData);
+    if (writeCategoryData) {
+      response.json({
+        status: "File write Error",
+        data: [],
+      });
+    } else {
+      response.json({
+        status: "success",
+        data: categoryDataObj,
+      });
+    }
+  })
+  .get((request, response) => {
+    const readCategoryData = fs.readFileSync("./data/categories.json", {
+      encoding: "utf-8",
+      flag: "r",
+    });
 
-        if (writeCategoryData) {
-            response.json({
-                status: 'File write Error',
-                data: []
-            })
-        } else {
-            response.json({
-                status: 'success',
-                data: categoryDataObj
-            })
-        }
-
+    response.json({
+      status: "success",
+      data: JSON.parse(readCategoryData),
+    });
+  })
+  .delete((request, response) => {
+    const body = request.body
+    const savedCategories = fs.readFileSync('./data/categories.json', {
+      encoding: 'utf-8',
+      flag: 'r',
     })
-    .get((request, response) => {
-        const readCategoryData = fs.readFileSync('./data/categories.json', {
-            encoding: 'utf-8',
-            flag: 'r'
-        })
+    const savedCategoriesObject = JSON.parse(savedCategories)
 
+    const filteredCategories = savedCategoriesObject.filter(
+      category => category.id != body.categoryId
+    )
+    console.log(filteredCategories.length)
+    fs.writeFileSync('./data/categories.json', JSON.stringify(filteredCategories))
+    response.json({
+      status: 'success',
+      data: filteredCategories
+    })
+  })
+  .put((request, response) => {
+    const body = request.body;
 
+    const catId = body.categoryId;
 
-        response.json({
-            status: 'success',
-            data: JSON.parse(readCategoryData)
-        })
+    const savedCategories = fs.readFileSync('./data/categories.json', {
+      encoding: 'utf-8',
+      flag: 'r'
     })
 
+    const savedCategoriesObjectArray = JSON.parse(savedCategories)
+    const foundCategory = savedCategoriesObjectArray.filter(category => category.id == catId)[0]
 
+    response.json({
+      status: 'success',
+      data: foundCategory
+    })
+  });
 
+app.get('/search', (request, response) => {
 
+  console.log(request.query)
+  const savedCategories = fs.readFileSync('./data/categories.json', {
+    encoding: 'utf-8',
+    flag: 'r',
+  })
 
+  const savedCategoriesArrayObject = JSON.parse(savedCategories);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+  const foundCategory = savedCategoriesArrayObject.filter(category =>
+    category.name.includes(request.query.value)
+  );
+
+  response.json({
+    status: 'success',
+    data: foundCategory
+  })
 })
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
