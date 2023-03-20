a955c8bbfcd668eb45650ed02e1c53b7eb88b99b
Morning <morning@mstars-lab2-10s-iMac.local>
2023-03-20 12:30:43 +0800

    day-79

---
+++ day-79/day-79-babel/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import helloRouter from './routes/api'
+
+
+const app = express()
+
+const PORT = 8081
+
+
+app.use(helloRouter)
+
+app.get('/', (request, response) => {
+    response.status(200).send(
+        '<h1> Day - 79 - babel</h1>'
+    )
+})
+
+app.listen(PORT, () => {
+    console.log(`Application is running on http://localhost:${PORT}`)
+})
