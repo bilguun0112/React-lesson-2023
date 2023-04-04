fa5b4c48bee712e7863c0b1fe24ff7c09dafbc9b
Morning <morning@mstars-lab2-10s-iMac.local>
2023-04-04 12:33:38 +0800

    day - 91 nextJS

---
+++ day-91-nextjs-typescript/be/controllers/theaterController.ts
@@ -0,0 +1,38 @@
+import TheaterModel from "../models/theathers.model";
+import { Request, Response } from "express";
+
+export const getTheaters = async (req: Request, res: Response) => {
+  try {
+    const theaters = await TheaterModel.find();
+    res.status(200).json(theaters);
+  } catch (error) {
+    res.status(404).json({ data: "dahin azaa uzeerei" });
+  }
+};
+
+export const getTheaterById = async (req: Request, res: Response) => {
+  try {
+    const theaters = await TheaterModel.find({
+      theaterId: Number(req.params.id),
+    });
+    res.status(200).json(theaters);
+  } catch (error) {
+    res.status(404).json({ data: [] });
+  }
+};
+
+export const searchTheaters = async (req: Request, res: Response) => {
+  const keyword: string = String(req.query.keyword);
+  try {
+    const theater = await TheaterModel.find({
+      $or: [
+        { "location.address.city": keyword },
+        { "location.address.steet1": keyword },
+        { "location.address.state": keyword },
+      ],
+    });
+    res.status(200).json(theater);
+  } catch (error) {
+    res.status(404).json({ data: [] });
+  }
+};
---
+++ day-91-nextjs-typescript/be/models/comment.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IComment {
+  name: string;
+  email: string;
+  movie_id: { type: mongoose.Schema.Types.ObjectId; ref: "Movies" };
+  text: string;
+  date: Date;
+}
+
+const CommentSchema: Schema = new Schema({});
+
+const CommentModel = mongoose.model<IComment>("Comment", CommentSchema);
+export default CommentModel;
