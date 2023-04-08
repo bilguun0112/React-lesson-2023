bb7be4037c8a1db8e0a4d8d7f17098a3b4542f8a
bilguun0112 <bilguunbayanmunkh10@gmail.com>
2023-04-08 17:39:39 +0800

    package.json conflict resolved

--- day-92-next-js-routing-ssr-scr-ssg/be/controllers/moviesController.ts
+++ day-92-next-js-routing-ssr-scr-ssg/be/controllers/moviesController.ts
@@ -3,12 +3,13 @@ import { Request, Response } from "express";
 
 export const getMovies = async (req: Request, res: Response) => {
   const page: number = Number(req.query.page) || 0;
-  const moviesPerPage: number = Number(req.query.moviesPerPage) || 120;
+  const moviesPerPage: number = Number(req.query.moviesPerPage) || 20;
 
   try {
     const movies = await MoviesModel.find()
       .limit(moviesPerPage)
-      .skip(moviesPerPage * page);
+      .skip(moviesPerPage * page)
+      .sort({ year: -1 });
     res.status(200).json(movies);
   } catch (error) {
     res.status(404).json({ data: "dahin azaa uzeerei" });
